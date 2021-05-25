32f7318081df3f5a50f6bbb423a762236866a5fb
Lydia <lychicherova@gmail.com>
2021-05-24 20:36:53 -0400

    add selecting, sorting and pagination using mongoose methods

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,11 +7,73 @@ const Bootcamp = require('../models/Bootcamp');
 // @route GET /api/v1/bootcamps
 // @access Public  
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  // Copy req.query
+  const reqQuery = {...req.query};
+
+  // fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // loop over removeFields and delete them from requestQuery
+
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // Create query strig
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, gte etc )
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+  
+
+  // finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if(req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort fields
+  if(req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page -1)*limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  // executing query
+  const bootcamps = await query;
+  
+  // pagination result
+
+  let pagination = {};
+  if (endIndex < total){
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+  if(startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
 
   res
   .status(200)
-  .json({ success: true, count: bootcamps.length, data: bootcamps})
+  .json({ success: true, count: bootcamps.length, pagination, data: bootcamps})
   
 });
 
